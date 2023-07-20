2e94cd6290d43de14aa83a16af8d9977177c026f
Shailja Gupta <115636055+shailja026@users.noreply.github.com>
2023-07-20 15:52:59 +0530

    done

---
+++ src/data/data.jsx
@@ -0,0 +1,35 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"Gym_website",
+        image:WorkIm,
+        github:"https://github.com/shailja026/Gym_website/tree/master",
+        live:"https://bright-chimera-e1fef3.netlify.app/",
+    },
+    {
+        id:2,
+        name:"Twitter_clone",
+        image:RealEst,
+        github:"https://github.com/shailja026/abcd/tree/tweet",
+        live:"https://peaceful-gaufre-7ba2da.netlify.app/",
+    },
+    {
+        id:3,
+        name:"Kanban-board ",
+        image:WorkIm,
+        github:"https://github.com/shailja026/kanban-board",
+        live:"https://rococo-snickerdoodle-36b4ce.netlify.app/",
+    },
+    {
+        id:4,
+        name:"Movie_Land",
+        image:RealEst,
+        github:"https://github.com/shailja026/movies_with_api",
+        live:"",
+    },
+
+
+]
